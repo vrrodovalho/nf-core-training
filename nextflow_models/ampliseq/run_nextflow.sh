3c0e86d84b25611c36cff69e863d42fbe5e8ffe2
Vinícius R Rodovalho <vrrodovalho@gmail.com>
2024-07-30 16:50:05 -0300

    Update run_nextflow.sh

--- nextflow_models/ampliseq/run_nextflow.sh
+++ nextflow_models/ampliseq/run_nextflow.sh
@@ -4,5 +4,7 @@ nextflow run nf-core/ampliseq \
     -params-file params.yaml \
     -profile docker \
     -r 2.10.0 \
-    --max_cpus 8 \
-    --max_memory '16.GB'
+    --max_memory '60.GB'
+
+#    --max_cpus 8 \
+#    --max_memory '16.GB'
